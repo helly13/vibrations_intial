282a1033dfe603e21e38a355a957418c28f928da
helly13 <thakkarhelly13@gmail.com>
2020-06-04 02:34:58 +0530

    Final changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LocalStorageService,SessionStorageService } from "angular-web-storage";
+import { Router } from "@angular/router";
+import { SponsorService } from "../services/sponsor.service";
+import { Sponsor } from '../classes/sponsor';
+
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -7,9 +13,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router,private sponsor_ser:SponsorService,private sessionst:SessionStorageService) { }
+
+  email_id:any;
+  flag:boolean;
+  sponsor_list:Sponsor[]=[];
 
   ngOnInit(): void {
+    this.email_id=this.sessionst.get('email_id');
+    if(this.email_id)
+    {
+      this.flag=false;
+    }
+    else
+    {
+      this.flag=true;
+    }
+
+    this.sponsor_ser.getSponsorList().subscribe(
+      (data:any[])=>{
+        this.sponsor_list=data;
+        console.log(this.sponsor_list);
+      }
+    );
+  }
+
+  logout()
+  {
+    this.sessionst.remove('email_id');
+    this.sessionst.remove('student_type');
+    this._router.navigate(['login']);
+    this.ngOnInit();
   }
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,19 +13,27 @@ export class HeaderComponent implements OnInit {
 
   constructor(private _router: Router,private localst:LocalStorageService,private sessionst:SessionStorageService,private evnt_ser:EvntService) { }
   flag:boolean;
-  timespan:any;
   email_id:any;
-  User_Name:any;
+  User_Name:string;
   ngOnInit(): void {
 
     this.email_id=this.sessionst.get('email_id');
-    console.log("header :: "+this.email_id);
+    // console.log("header :: "+this.email_id);
+
     if(this.email_id)
     {
       this.flag=false;
       this.evnt_ser.getStudentDetailsById(this.email_id).subscribe(
         (data:any)=>{
           this.User_Name=data[0].name;
+          if(this.User_Name.indexOf(' ')>0)
+          {
+            this.User_Name=this.User_Name.substring(0,this.User_Name.indexOf(' '))
+          }
+          else
+          {
+            this.User_Name=this.User_Name;
+          }
         }
       );
     }
